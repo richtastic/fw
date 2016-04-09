b46840acaf1fa97acd88d67bd669388a1dfcf363
Richie <richard.zirbes@disney.com>
2016-04-08 17:38:23 -0700

    virtualbox symlinks and copying are a mess

---
+++ simple/simple.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package simple
+ * @version 0.0.0
+ */
+/*
+Plugin Name: simple
+Description: simple example plugin
+Author: Simple
+Version: 0.0.0
+License: Apache License Version 2.0 | http://www.apache.org/licenses/LICENSE-2.0.txt
+*/
+
+$SIMPLE_PLUGIN_ROOT_PATH = dirname(__FILE__);
+
+error_log("Simple Loaded: ".$SIMPLE_PLUGIN_ROOT_PATH);
+
+
+?>
